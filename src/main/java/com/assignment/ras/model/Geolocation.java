5d6ba0b245b3b12258a16425aba48abdd2a7e586
swkasuk <145101877+swkasuk@users.noreply.github.com>
2023-09-20 11:09:25 -0400

    Update Geolocation.java

--- src/main/java/com/assignment/ras/model/Geolocation.java
+++ src/main/java/com/assignment/ras/model/Geolocation.java
@@ -6,12 +6,13 @@ import java.math.BigDecimal;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import lombok.NoArgsConstructor;
 
-import lombok.Builder;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(value = Include.NON_NULL)
-@Builder
+@NoArgsConstructor
+
 public class Geolocation implements Serializable {
 
 	/**
