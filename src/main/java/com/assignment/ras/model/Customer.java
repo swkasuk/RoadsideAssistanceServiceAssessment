5153a87b78e963db3d23a5ae6a69e63789f591ae
swkasuk <145101877+swkasuk@users.noreply.github.com>
2023-09-20 11:07:22 -0400

    Update Customer.java

--- src/main/java/com/assignment/ras/model/Customer.java
+++ src/main/java/com/assignment/ras/model/Customer.java
@@ -6,11 +6,13 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
-import lombok.Builder;
+import lombok.NoArgsConstructor;
+
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(value = Include.NON_NULL)
-@Builder
+@NoArgsConstructor
+
 public class Customer extends Geolocation implements Serializable {
 
 	/**
